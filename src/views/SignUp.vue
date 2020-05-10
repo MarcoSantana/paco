5b657c5830e937a6e99fd27dd618edc7dcbcd6f5
Marco Antonio Santana <msantana@pop-os.localdomain>
2020-05-10 11:26:23 -0500

    Added some filds

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="page-wrapper">
+  <div class="page-wrapper" background-color="primary">
     <!-- Loader -->
     <div v-show="user === undefined" data-test="loader">Authenticating...</div>
 
@@ -9,16 +9,50 @@
     </div>
 
     <p v-if="loginError">{{ loginError }}</p>
+    <p v-if="apiError">{{ apiError }}</p>
     <!-- Auth UI -->
-    <div v-show="user !== undefined && !user && networkOnLine" data-test="signup-form-container">
+    <div
+      v-show="user !== undefined && !user && networkOnLine"
+      class="signup-form-container"
+      id="signup-form-container"
+      data-test="signup-form-container"
+    >
       <div id="login-box">
         <div class="left">
           <h1>Registrarse</h1>
 
-          <input type="text" name="username" placeholder="Username" />
-          <input type="text" name="email" placeholder="E-mail" />
-          <input type="password" name="password" placeholder="Password" />
-          <input type="password" name="password2" placeholder="Retype password" />
+          <input type="text" name="email" placeholder="E-mail" id="registration-email" data-test="registration-email" />
+          <input
+            type="text"
+            name="email"
+            placeholder="Confirme su e-mail"
+            id="registration-email-confirmation"
+            data-test="registration-email-confirmation"
+          />
+          <input
+            type="text"
+            name="name"
+            placeholder="Nombres (ej. Juan Carlos)"
+            id="registration-name"
+            data-test="registration-name"
+          />
+          <input
+            type="text"
+            name="registration-lastname"
+            placeholder="Apellido Paterno (ej. González)"
+            id="registration-lastname-1"
+            data-test="registration-lastname-1"
+          />
+          <input type="password" name="password" placeholder="Contraseña" />
+          <input type="password" name="password2" placeholder="Confirme su contraseña" />
+
+          <input
+            type="text"
+            name="license"
+            placeholder="Cédula profesional de licenciatura en medicina"
+            id="registration-license"
+            disabled="disabled"
+          />
 
           <input type="submit" name="signup_submit" value="Sign me up" />
         </div>
@@ -35,6 +69,7 @@
     </div>
     <!-- signup-form-container -->
   </div>
+  <!-- page wrapper -->
 </template>
 
 <script>
@@ -42,10 +77,9 @@ import { mapState, mapMutations } from 'vuex'
 import { isNil } from 'lodash'
 import firebase from 'firebase/app'
 import { desktop as isDekstop } from 'is_js'
-// import axios from 'axios'
 
 export default {
-  data: () => ({ loginError: null }),
+  data: () => ({ loginError: null, apiError: null }),
   head() {
     return {
       title: {
@@ -75,34 +109,8 @@ export default {
       immediate: true,
     },
   },
-  mounted: () => {
-    // axios()
-    //   .get(
-    //     'https://cors-anywhere.herokuapp.com/http://search.sep.gob.mx/solr/cedulasCore/select?fl=%2A%2Cscore&q=danelia+gonzalez+camnpuzano&start=0&rows=1&facet=true&indent=on&wt=json'
-    //   )
-    //   .then(response => {
-    //     console.log(response)
-    //   })
-    fetch(
-      'https://cors-anywhere.herokuapp.com/http://search.sep.gob.mx/solr/cedulasCore/select?fl=%2A%2Cscore&q=danelia+gonzalez+camnpuzano&start=0&rows=10&facet=true&indent=on&wt=json'
-    )
-      .then(function(response) {
-        return response.json()
-      })
-      .then(function(myJson) {
-        console.log('docs', myJson.response.docs)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-    // axios
-    //   .get(
-    //     'http://search.sep.gob.mx/solr/cedulasCore/select?fl=%2A%2Cscore&q=danelia+gonzalez+camnpuzano&start=0&rows=1&facet=true&indent=on&wt=json',
-    //     { crossdomain: true }
-    //   )
-    //   .then(response => {
-    //     console.log(response.data)
-    //   })
+  mounted() {
+    this.documentAPI()
   },
   methods: {
     ...mapMutations('authentication', ['setUser']),
@@ -123,6 +131,21 @@ export default {
         this.loginError = err
         this.setUser(null)
       }
+    }, // login
+    async documentAPI() {
+      fetch(
+        `https://cors-anywhere.herokuapp.com/http://search.sep.gob.mx/solr/cedulasCore/select?fl=%2A%2Cscore&q=danelia+gonzalez+camnpuzano&start=0&rows=10&facet=true&indent=on&wt=json`
+      )
+        .then(function(response) {
+          return response.json()
+        })
+        .then(function(myJson) {
+          console.log('docs', myJson.response.docs)
+        })
+        .catch(error => {
+          console.log(error)
+          this.apiError = error
+        })
     },
   },
 }
@@ -143,12 +166,16 @@ body {
   font-weight: 300;
 }
 
+#signup-form-container {
+  box-sizing: border-box;
+}
+
 #login-box {
   position: relative;
-  margin: 5% auto;
+  box-sizing: border-box;
+  margin: 5% 100px 5% 100px;
   width: 600px;
-  height: 400px;
-  background: #fff;
+  height: 800px;
   border-radius: 2px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
 }
@@ -160,7 +187,8 @@ body {
   box-sizing: border-box;
   padding: 40px;
   width: 300px;
-  height: 400px;
+  // height: 400px;
+  height: auto;
 }
 
 h1 {
@@ -187,7 +215,7 @@ input[type='password'] {
 
 input[type='text']:focus,
 input[type='password']:focus {
-  border-bottom: 2px solid #16a085;
+  border-bottom: 2px solid #16a085; //Cahnge me
   color: #16a085;
   transition: 0.2s ease;
 }
@@ -200,7 +228,7 @@ input[type='submit'] {
   border: none;
   border-radius: 2px;
   color: #fff;
-  font-family: 'Roboto', sans-serif;
+  font-family: 'Roboto', sans-serif; // Change me
   font-weight: 500;
   text-transform: uppercase;
   transition: 0.1s ease;
