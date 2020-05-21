9204b76405bff79c682bc6f7c7e3fe9cb5b31024
Marco A. Santana <marco.santana@gmail.com>
2020-05-21 10:23:06 -0500

    Update validation.js

--- src/misc/validation.js
+++ src/misc/validation.js
@@ -13,8 +13,6 @@ Every error message must begin with 'Este campo es obligatorio / Este campo debe
 import { extend } from 'vee-validate'
 import { required, email, numeric, confirmed } from 'vee-validate/dist/rules'
 
-this.$validator.localize('es')
-
 extend('email', {
   ...email,
   message: 'Este campo debe ser un correo electrónico válido',
@@ -38,10 +36,21 @@ extend('numeric', {
 extend('length', {
   validate(value, { min, max }) {
     const string = value.toString()
-    if (string.length > min && string.length < max) {
+    if (string.length >= min && string.length <= max) {
       return true
     }
     return 'Este campo debe ser de máximo {max} y mínimo {min} caracteres'
   },
   params: ['min', 'max'],
+})
+
+extend('strong_password', {
+  validate(value) {
+    const string = value.toString()
+    const strongRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})')
+    if (strongRegex.test(string)) {
+      return true
+    }
+    return 'La contraseña debe contener al menos: una letra mayúscula, una letra minúscula y un número'
+  },
 })
