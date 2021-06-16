9cdc56902f8e8e40a6d6a0aab61ac60da7a212af
MarcoSantana <marco.santana@gmail.com>
2021-06-15 19:09:25 -0500

    Prep for curriculum refactor

---
+++ src/views/Curriculum.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-wrapper">
+    Here be curriculum
+  </div>
+</template>
+<script>
+// import { mapGetters } from 'vuex'
+// TODO import the component to show the rendered curriculum or the creation form
+export default {
+  // TODO components Curriculum form
+  computed: {
+    // TODO curriculum/userDetails store
+    // ...mapGetters('products', ['getProductById']),
+    // TODO refactor to get the current user curriculum
+    // currentProduct() {
+    //   return this.getProductById(this.id)
+    // },
+  },
+}
+</script>
+
