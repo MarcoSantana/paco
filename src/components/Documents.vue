06244c138ffc3c08803e0f80e294e924b05608b3
MarcoSantana <marco.santana@gmail.com>
2021-07-22 19:17:24 -0500

    Removed (temporary) add document component

--- src/components/Documents.vue
+++ src/components/Documents.vue
@@ -2,17 +2,18 @@
   <div class="page-wrapper">
     <h1 class="documents-page-title">Página de Documentos</h1>
     <document-list class="document-list"></document-list>
-    <add-document v-if="networkOnLine"></add-document>
+    <!-- <add-document v-if="networkOnLine"></add-document> -->
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import DocumentList from '@/components/DocumentList'
-import AddDocument from '@/components/AddDocument'
+// import AddDocument from '@/components/AddDocument'
 
 export default {
-  components: { DocumentList, AddDocument },
+  // components: { DocumentList, AddDocument },
+  components: { DocumentList },
   computed: mapState('app', ['networkOnLine']),
 }
 </script>
