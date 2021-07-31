33960c3cc6ab78796ab0eac1e677223411f301aa
MarcoSantana <marco.santana@gmail.com>
2021-07-31 12:12:50 -0500

    Completed UI

--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -5,11 +5,54 @@
       href="/home?mode=resetPassword&oobCode=tRiYs5S8Ry5pwQrd7SH5-qQr9Zlv7Hkg2yzX9HyQyhIAAAF6-Fh59g&apiKey=AIzaSyAge4gR03bNsbvhOtyRSuw4qtGBgE4pX_U&lang=es-419"
       >Link</a
     >
-    <div>Mensajes:>> {{ accountEmail }}</div>
+    <div v-if="accountEmail">:>> {{ accountEmail }}</div>
     <div>
-      <input type="text" placeholder="nueva contraseña" required />
-      <input type="text" placeholder="confirme contraseña" required />
-      <button disabled="disabled" class="button">Cambiar contraseña</button>
+      <validation-observer v-slot="{ invalid }">
+        <div>
+          La contraseña debe contener al menos: 8 caracteres y un máximo de 16, una letra mayúscula, una letra minúscula
+          y un número
+        </div>
+        <validation-provider
+          v-slot="{ errors }"
+          rules="required|length:8,16|strong_password"
+          name="reset-password-valitator"
+        >
+          <span name="new-password-span" :class="{ error: errors[0] }">
+            <label for="newPassword" class="tip">Nueva contraseña</label>
+            <div class="input-container">
+              <span>{{ errors[0] }}</span>
+              <i class="mdi mdi-lock icon"></i>
+              <input
+                id="new-password"
+                v-model="newPassword"
+                type="text"
+                name="newPassword"
+                placeholder="Nueva contraseña"
+                data-test="password-reset"
+              />
+            </div>
+          </span>
+        </validation-provider>
+        <!-- email -->
+        <validation-provider v-slot="{ errors }" rules="confirmed:reset-password-valitator">
+          <span name="new-password-confirmation-span" :class="{ error: errors[0] }">
+            <label for="newPasswordConfirmation" class="tip">Confirmación de contraseña</label>
+            <div class="input-container">
+              <span>{{ errors[0] }}</span>
+              <i class="mdi mdi-lock icon"></i>
+              <input
+                id="new-password-confirmation"
+                v-model="newPasswordConfirmation"
+                type="text"
+                name="newPasswordConfirmation"
+                placeholder="Confirme su nueva contraseña"
+                data-test="password-reset-confirmation"
+              />
+            </div>
+          </span>
+        </validation-provider>
+        <!-- password-confirmation -->
+      </validation-observer>
     </div>
   </div>
 </template>
@@ -17,6 +60,7 @@
 <script>
 import { auth } from 'firebase'
 import { isNil } from 'lodash'
+import * as firebaseErrors from '@/firebase/firebase-errors.json'
 
 export default {
   // TODO import the errors translation 202107.30-20.47
@@ -25,8 +69,17 @@ export default {
     resetLang: String,
     resetKey: String,
   },
-  data: () => ({ accountEmail: null }),
+  data: () => ({
+    accountEmail: null,
+    invalid: false,
+    translatedError: null,
+    errors: [],
+    newPassword: null,
+    newPasswordConfirm: null,
+    newPasswordConfirmation: null,
+  }),
   mounted() {
+    // only to load it in dev
     this.handleResetPassword()
   },
   methods: {
@@ -37,8 +90,12 @@ export default {
           this.accountEmail = email
         })
         .catch(error => {
-          this.accountEmail = error
+          const fbErr = Object.values(firebaseErrors)[0]
+          this.translatedError = fbErr[error.code]
         })
+      // .catch(error => {
+      //   this.accountEmail = error
+      // })
       if (!isNil(this.accountEmail) && !isNil(this.newPasword))
         auth().confirmPasswordReset(this.resetActionCode, this.newPassword)
     },
@@ -49,4 +106,15 @@ export default {
 <style lang="scss" scoped>
 @import '@/theme/style.scss';
 @import '@/theme/variables.scss';
+.error {
+  color: $danger-color;
+  input {
+    background-color: lighten($color: $danger-color, $amount: 20%);
+    opacity: 0.6;
+    border-radius: 10px;
+  }
+  span {
+    color: $danger-color;
+  }
+}
 </style>
