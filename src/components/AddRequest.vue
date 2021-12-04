2455a944f70a634f42844d668321876cb812098b
MarcoSantana <marco.santana@gmail.com>
2021-12-04 12:41:47 -0600

    Refactored to separate components

--- src/components/AddRequest.vue
+++ src/components/AddRequest.vue
@@ -43,10 +43,10 @@
         <li>Donativo no reembolsable de $ 5,700. 00/100 m.n.</li>
       </ol>
     </div>
-
+    <!-- TODO populate this with existing dat if any 202112.04-12.31 -->
     <v-stepper v-model="curr" color="green">
       <v-stepper-content v-for="(step, n) in steps" :key="n" :step="n + 1">
-        <!-- //TODO the header looks like shit 202112.02-17.13 -->
+        //TODO the header looks like shit 202112.02-17.13
         <v-stepper-header class="overflow-x-auto mb-3">
           <v-stepper-step
             :complete="stepComplete(n + 1)"
@@ -56,58 +56,9 @@
             >{{ step.name }}</v-stepper-step
           >
         </v-stepper-header>
-        <v-card class="mx-auto" max-width="500">
-          <v-fade-transition>
-            <!-- // TODO Add a better lazy-src 202112.02-17.14 -->
-            <v-img
-              v-if="files[n] && (files[n].type === 'image/png' || files[n].type === 'image/jpeg')"
-              class="ma-5"
-              contain
-              lazy-src="https://picsum.photos/id/11/10/6"
-              max-height="150"
-              max-width="250"
-              :src="getURL(files[n])"
-            />
-          </v-fade-transition>
-          <v-fade-transition>
-            <object
-              v-if="files[n] && files[n].name && files[n].type === 'application/pdf'"
-              class="ma-5"
-              style="max-width: 500px; min-width: 344px; min-height: 500px;"
-              :data="getURL(files[n])"
-            />
-          </v-fade-transition>
-          <v-card-text v-if="step.upload">
-            <v-form :ref="'stepForm'" v-model="step.valid" lazy-validation>
-              <v-file-input
-                v-if="step.upload"
-                :ref="step.refName"
-                v-model="files[n]"
-                :rules="step.rules"
-                accept="image/png, image/jpeg, application/pdf"
-                :placeholder="step.placeholder"
-                counter
-                show-size
-                truncate-length="15"
-                required
-              />
-            </v-form>
-            <v-alert v-if="documentCreationMessage.message" dense text :type="documentCreationMessage.type">
-              {{ documentCreationMessage.message }}
-            </v-alert>
-            <v-btn
-              v-if="documentCreationMessage.type !== 'success'"
-              color="success"
-              :disabled="!step.valid"
-              @click="validate(n)"
-            >
-              Guardar
-              <v-icon right dark>
-                mdi-cloud-upload
-              </v-icon>
-            </v-btn>
-          </v-card-text>
-        </v-card>
+        <v-form :ref="'stepForm'" v-model="step.valid" lazy-validation>
+          <upload-document :document="step"></upload-document>
+        </v-form>
         <v-btn v-if="n + 1 < steps.length + 1" color="primary" :disabled="!step.valid" @click="nextStep(n)"
           >Continuar</v-btn
         >
@@ -121,6 +72,7 @@
 <script>
 import { mapState, mapActions, mapGetters, mapMutations } from 'vuex'
 import { isNil } from 'lodash'
+import UploadDocument from '@/components/UploadDocument.vue'
 
 // TODO move this elsewhere 202112.02-17.22
 // Rules
@@ -129,6 +81,7 @@ const maxSize = (value, max) => !value || value.size < max || `El archivo no pue
 // const minSize = (value, min) => !value || value.size < min || `El archivo no puede ser menor a los ${min / 1000000} Mb`
 
 export default {
+  components: { UploadDocument },
   props: {
     id: {
       type: String,
@@ -144,7 +97,7 @@ export default {
         name: 'Copia del título y cédula profesional de la licenciatura en medicina.',
         upload: true,
         // TODO separate this into functions to reuse them
-        rules: [value => maxSize(value, 2000000), value => required(value)],
+        rules: [value => required(value), value => maxSize(value, 2000000)],
         placeholder: 'Título profesional',
         refName: 'titulo',
         valid: false,
@@ -187,7 +140,6 @@ export default {
   },
   watch: {},
   mounted() {
-    // this.setFormNameToCreate('Solicitud de examen')
     this.setEvent()
   },
   methods: {
@@ -196,16 +148,6 @@ export default {
     ...mapActions('events', ['setUserEvent']),
     ...mapActions('documents', ['createUserDocument']),
 
-    // <!--TODO: Add a method/action to update the document(fb) onModify (or whatever its called)-->
-    // ...mapMutations('forms', ['setFormNameToCreate']),
-    // ...mapActions('forms', ['triggerAddCurrentFormAction', 'triggerAddFormAction']),
-    // ...mapGetters('forms', ['getCurrentForm']),
-    // getAttrs(vnode) {
-    //   return get(vnode[0], 'attributes.input.data-test', {})
-    // },
-    // updateCurrentForm() {
-    //   this.triggerAddCurrentFormAction(this.model)
-    // },
     saveDocument(n) {
       this.setDocumentNameToCreate(this.step[n].name)
       this.triggerAddDocumentAction(this.formModel)
