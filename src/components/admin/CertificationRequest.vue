31fe5db503096e6e58a88a77a576add06a8f6da9
MarcoSantana <marco.santana@gmail.com>
2022-02-26 10:57:07 -0600

    Added vuetify and some order

--- src/components/admin/CertificationRequest.vue
+++ src/components/admin/CertificationRequest.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="details-container">
-    <div>
-      <h3>Detalles del aspirante</h3>
-      <v-card>
-        <div>Nombres: {{ user.firstName }} {{ user.lastName1 }} {{ user.lastName2 ? user.lastName2 : '' }}</div>
-        <div>
+  <div class="container">
+    <v-card>
+      <v-card-title class="text-h5 white--text justify-center mb-2 indigo lighten-3">
+        Detalles del aspirante</v-card-title
+      >
+      <v-list>
+        <v-list-item class="text-center">
           <document-file
             class="document-file"
             :document-name="documentName"
@@ -12,55 +13,104 @@
             :scrollable-modal="true"
             :resizable-modal="true"
             file-name="avatar"
-          ></document-file>
-        </div>
-        <div class="row">
-          <div>Fecha de nacimiento: {{ user.dob | intlDate }}</div>
-          <div>
+          />
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>{{ $t('name') | capitalize }}:</v-list-item-title
+          ><v-list-item-subtitle>
+            {{ user.firstName | capitalize }} {{ user.lastName1 | capitalize }}
+            {{ user.lastName2 ? user.lastName2 : '' | capitalize }}
+          </v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Fecha de nacimiento:</v-list-item-title>
+          <v-list-item-subtitle>
+            {{ user.dob | intlDate }}
             <small>Edad: {{ user.dob | age }}</small>
-          </div>
-        </div>
-        <div>País de nacimiento: {{ user.nacionality }}</div>
-        <div>Género: {{ user.gender }} <i :class="user.gender | genderize"></i></div>
-        <div>CURP: {{ user.curp }}</div>
-        <div>RFC: {{ user.rfc }}</div>
-        <h4>Datos de contacto</h4>
-        <div>Correo electrónico: {{ user.contact.email }}</div>
-        <div>Teléfono celular: {{ user.contact.cellphone }}</div>
-        <div>Fax: {{ user.contact.fax }}</div>
-        <div>Teléfono: {{ user.contact.telephone }}</div>
-        <div>Dirección: {{ user.address }}</div>
-      </v-card>
-      <h3>Detalles de solicitud</h3>
-      <div>
-        <div>{{ request.address }} {{ request.date | intlDate }}</div>
-        <div>Examen previo: {{ request.previousExam }}</div>
-      </div>
-      <h3>Recibo de pago</h3>
-      <div>
-        <document-file
-          class="document-file"
-          :document-name="documentName"
-          :user-id="document.userId"
-          :scrollable-modal="true"
-          :resizable-modal="true"
-          file-name="voucher"
-        ></document-file>
-      </div>
-      <h3>Estudios de licenciatura de medicina</h3>
-      <div>
-        <div>Título de la licenciatura en medicina</div>
-        <document-file
-          class="document-file"
-          :document-name="document.name"
-          :user-id="document.userId"
-          :scrollable-modal="true"
-          :resizable-modal="true"
-          file-name="degreeDiploma"
+          </v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item
+          ><v-list-item-title>Lugar de nacimiento:</v-list-item-title>
+          <v-list-item-subtitle>{{ user.nacionality }}</v-list-item-subtitle></v-list-item
         >
-        </document-file>
-        <div>
-          <div>Documento del ENARM (constancia de haber presentado o equivalente)</div>
+        <v-list-item
+          ><v-list-item-title>Género:</v-list-item-title>
+          <v-list-item-subtitle>{{ user.gender }} <i :class="user.gender | genderize"></i> </v-list-item-subtitle
+        ></v-list-item>
+        <v-list-item>
+          <v-list-item-title>CURP:</v-list-item-title>
+          <v-list-item-subtitle>{{ user.curp }}</v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item
+          ><v-list-item-title>RFC:</v-list-item-title>
+          <v-list-item-subtitle>{{ user.rfc }}</v-list-item-subtitle></v-list-item
+        >
+        <v-divider></v-divider>
+        <v-list-item class="text-center"><v-list-item-title>Datos de contacto</v-list-item-title></v-list-item>
+        <v-list-item
+          ><v-list-item-title>Correo electrónico: </v-list-item-title
+          ><v-list-item-subtitle>{{ user.contact.email }}</v-list-item-subtitle></v-list-item
+        >
+        <v-list-item
+          ><v-list-item-title>Teléfono celular: </v-list-item-title
+          ><v-list-item-subtitle>{{ user.contact.cellphone }}</v-list-item-subtitle></v-list-item
+        >
+        <v-list-item
+          ><v-list-item-title>Fax: </v-list-item-title
+          ><v-list-item-subtitle>{{ user.contact.fax }}</v-list-item-subtitle></v-list-item
+        >
+        <v-list-item
+          ><v-list-item-title>Teléfono: </v-list-item-title
+          ><v-list-item-subtitle>{{ user.contact.telephone }}</v-list-item-subtitle></v-list-item
+        >
+        <v-list-item
+          ><v-list-item-title>Dirección: </v-list-item-title
+          ><v-list-item-subtitle>{{ user.address }}</v-list-item-subtitle></v-list-item
+        >
+        <v-divider />
+        <v-list-item><v-list-item-title class="text-center">Detalles de solicitud</v-list-item-title></v-list-item>
+        <v-list-item>
+          <v-list-item-title>Lugar de la solicitud: </v-list-item-title>
+          <v-list-item-subtitle>{{ request.address }}</v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item
+          ><v-list-item-title>Fecha de la solicitud</v-list-item-title
+          ><v-list-item-subtitle>{{ request.date | intlDate }}</v-list-item-subtitle></v-list-item
+        >
+        <v-list-item
+          ><v-list-item-title>Examen previo: </v-list-item-title
+          ><v-list-item-subtitle>{{ request.previousExam }}</v-list-item-subtitle></v-list-item
+        >
+        <v-divider />
+        <v-list-item class="text-center"><v-list-item-title>Recibo de pago</v-list-item-title></v-list-item>
+        <v-list-item class="text-center">
+          <document-file
+            class="document-file"
+            :document-name="documentName"
+            :user-id="document.userId"
+            :scrollable-modal="true"
+            :resizable-modal="true"
+            file-name="voucher"
+          ></document-file>
+        </v-list-item>
+        <v-divider />
+        <v-list-item
+          ><v-list-item-title class="text-center">Estudios de licenciatura de medicina</v-list-item-title></v-list-item
+        >
+        <v-list-item><v-list-item-title>Título de la licenciatura en medicina</v-list-item-title></v-list-item>
+        <v-list-item class="text-center"
+          ><document-file
+            class="document-file"
+            :document-name="document.name"
+            :user-id="document.userId"
+            :scrollable-modal="true"
+            :resizable-modal="true"
+            file-name="degreeDiploma"
+        /></v-list-item>
+        <v-list-item>
+          <v-list-item-title>Documento del ENARM (constancia de haber presentado o equivalente)</v-list-item-title>
+        </v-list-item>
+        <v-list-item class="text-center">
           <document-file
             class="document-file"
             :document-name="document.name"
@@ -70,13 +120,15 @@
             file-name="enarm"
           >
           </document-file>
-        </div>
-      </div>
-      <div>
-        <h4>Estudios de posgrado</h4>
-        <div>
-          <div>Cédula profesional: {{ request.postgraduate.license }}</div>
-          <div>Cédula</div>
+        </v-list-item>
+        <v-divider />
+        <v-list-item class="text-center"><v-list-item-title>Estudios de posgrado</v-list-item-title></v-list-item>
+        <v-list-item
+          ><v-list-item-title>Cédula profesional: </v-list-item-title
+          ><v-list-item-subtitle>{{ request.postgraduate.license }}</v-list-item-subtitle></v-list-item
+        >
+        <v-list-item class="text-center"><v-list-item-title>Cédula</v-list-item-title></v-list-item>
+        <v-list-item class="text-center">
           <document-file
             class="document-file"
             :document-name="document.name"
@@ -86,37 +138,63 @@
             file-name="license"
           >
           </document-file>
-          <div>Universidad</div>
-          <div>Especialidad: {{ request.postgraduate.specialty }}</div>
-          <div>Hospital formativo: {{ request.postgraduate.hospital }}</div>
-          <div>Fecha de graduación: {{ request.postgraduate.graduationDate | intlDate }}</div>
-          <div>
-            <div>Constancia de residencia progresiva hospitalaria</div>
-            <document-file
-              class="document-file"
-              :document-name="document.name"
-              :user-id="document.userId"
-              file-name="pediatricResidence"
-            ></document-file>
-          </div>
-        </div>
-        <div>
-          <div>Diploma institucional en Medicina de Urgencias o en su caso Urgencias Pediatricas</div>
+        </v-list-item>
+        <v-divider />
+        <v-list-item
+          ><v-list-item-title>Especialidad: </v-list-item-title
+          ><v-list-item-subtitle>{{ request.postgraduate.specialty }}</v-list-item-subtitle></v-list-item
+        >
+        <v-list-item
+          ><v-list-item-title>Hospital formativo: </v-list-item-title
+          ><v-list-item-subtitle>{{ request.postgraduate.hospital }}</v-list-item-subtitle></v-list-item
+        >
+        <v-list-item
+          ><v-list-item-title>Fecha de graduación: </v-list-item-title
+          ><v-list-item-subtitle>{{
+            request.postgraduate.graduationDate | intlDate
+          }}</v-list-item-subtitle></v-list-item
+        >
+        <v-divider />
+        <v-list-item class="text-center"
+          ><v-list-item-title>Constancia de residencia progresiva hospitalaria</v-list-item-title></v-list-item
+        >
+        <v-list-item class="text-center">
+          <document-file
+            class="document-file"
+            :document-name="document.name"
+            :user-id="document.userId"
+            file-name="pediatricResidence"
+          ></document-file>
+        </v-list-item>
+        <v-list-item class="text-center"
+          ><v-list-item-title
+            >Diploma institucional en Medicina de Urgencias o en su caso Urgencias Pediatricas</v-list-item-title
+          ></v-list-item
+        >
+        <v-list-item class="center">
           <document-file
             class="document-file"
             :document-name="document.name"
             :user-id="document.userId"
             file-name="postgraduateDiploma"
           ></document-file>
-        </div>
-      </div>
-      <div>
-        <h4>Práctica profesional</h4>
-        <div>Hospital donde labora: {{ request.professionalExercise.hospital }}</div>
-        <div>Dirección: {{ request.professionalExercise.location }}</div>
-        <div>Cargo: {{ request.professionalExercise.charge }}</div>
-      </div>
-    </div>
+        </v-list-item>
+        <v-divider />
+        <v-list-item><v-list-item-title>Práctica profesional</v-list-item-title></v-list-item>
+        <v-list-item
+          ><v-list-item-title>Hospital donde labora: </v-list-item-title
+          ><v-list-item-subtitle>{{ request.professionalExercise.hospital }}</v-list-item-subtitle></v-list-item
+        >
+        <v-list-item
+          ><v-list-item-title>Dirección: </v-list-item-title
+          ><v-list-item-subtitle>{{ request.professionalExercise.location }}</v-list-item-subtitle></v-list-item
+        >
+        <v-list-item
+          ><v-list-item-title>Cargo: </v-list-item-title
+          ><v-list-item-subtitle>{{ request.professionalExercise.charge }}</v-list-item-subtitle></v-list-item
+        >
+      </v-list>
+    </v-card>
   </div>
 </template>
 
