360aeb5010ed3edad8b8d0937e7791b71673028d
MarcoSantana <marco.santana@gmail.com>
2022-04-11 11:06:50 -0500

    Finished

--- src/views/admin/dashboard/RegisteredUsers.vue
+++ src/views/admin/dashboard/RegisteredUsers.vue
@@ -1,38 +1,34 @@
 <template>
-  <v-card class="d-flex flex-column" max-width="400" height="400">
-    <v-card-title>
-      <v-icon large left>
-        mdi-file-multiple
-      </v-icon>
-      <span class="text-h6 font-weight-light">Usuarios registrados</span>
-    </v-card-title>
-    <v-spacer></v-spacer>
-    <v-card-text class="text-h5 font-weight-bold" style="height: 200;">
-      <p class="text-h1 text-center">{{ registeredUsers.count }}</p>
-      <p class="caption text-center">Ultima actualizacion {{ registeredUsers.updateTimestamp | intlDate }}</p>
-    </v-card-text>
-    <v-spacer></v-spacer>
-    <v-card-actions>
-      <v-list-item class="grow" to="/users">
-        <v-list-item-content>
-          <v-list-item-title>Ver detalle</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-card-actions>
-  </v-card>
+  <v-sheet>
+    <v-card v-if="registeredUsers" class="d-flex flex-column" max-width="400" height="400">
+      <v-card-title>
+        <v-icon large left>
+          mdi-file-multiple
+        </v-icon>
+        <span class="text-h6 font-weight-light text-capitalize">{{ $t('registeredUsers') }}</span>
+      </v-card-title>
+      <v-spacer></v-spacer>
+      <v-card-text class="text-h5 font-weight-bold" style="height: 200;">
+        <p class="text-h1 text-center">{{ registeredUsers.count }}</p>
+        <p class="caption text-center text-capitalize">
+          {{ $t('lastRecord') }} {{ registeredUsers.updateTimestamp | intlDate }}
+        </p>
+      </v-card-text>
+      <v-spacer></v-spacer>
+    </v-card>
+    <v-skeleton-loader v-else class="d-flex flex-column" max-width="400" height="400" type="card"></v-skeleton-loader>
+  </v-sheet>
 </template>
 <script>
 import CountersDB from '@/firebase/counters-db'
 import intlDate from '@/filters/intlDate'
 
 export default {
+  filters: { intlDate },
   data: () => ({}),
-  filters:{intlDate},
   asyncComputed: {
     async registeredUsers() {
-      // return async () => new CountersDB().readAll()
       const [counter] = await new CountersDB().readAll()
-      console.log('counter', counter.count)
       return counter
     },
   },
