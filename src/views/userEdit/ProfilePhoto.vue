d0174241241aa56702c1f394d699eff78f4ce12f
MarcoSantana <marco.santana@gmail.com>
2022-05-09 13:29:15 -0500

    Cleanup

--- src/views/userEdit/ProfilePhoto.vue
+++ src/views/userEdit/ProfilePhoto.vue
@@ -1,9 +1,9 @@
 <template>
   <v-container>
-    <v-card class="mx-auto" max-width="200">
+    <v-card class="mx-auto" max-width="200" outlined>
       <v-img :src="model.photoURL" max-width="200px" max-height="300px" />
       <v-card-text class="pt-6" style="position: relative;">
-        <v-btn absolute color="info" class="white--text" fab medium right top>
+        <v-btn absolute color="info" class="white--text" fab medium right top @click.stop="dialog = true">
           <v-icon>mdi-camera</v-icon>
         </v-btn>
       </v-card-text>
@@ -15,6 +15,18 @@
         {{ model.lastname2 | capitalize }}
       </v-card-title>
     </v-card>
+    <v-dialog v-model="dialog" width="500px" max-height="800px">
+      <v-card class="mx-auto" max-width="500px" max-height="800px">
+        <v-app-bar color="primary" dark>
+          <v-app-bar-title>Cambiar fotografía de perfil</v-app-bar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon color="error" @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-app-bar>
+        <photo-upload></photo-upload>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 <script src="./profilePhoto.comp.js" />
--- src/views/userEdit/profilePhoto.comp.js
+++ src/views/userEdit/profilePhoto.comp.js
@@ -1,8 +1,33 @@
+import { storage } from 'firebase'
+import { mapGetters } from 'vuex'
+import { isNil } from 'lodash'
+import PhotoUpload from './PhotoUpload.vue'
+
 export default {
+  components: { PhotoUpload },
+  mounted() {
+    console.log('this.user', this.user)
+  },
   props: { model: { type: Object, required: true } },
   data() {
     return {
-      fizz: 'buzz',
+      dialog: false,
+      fileURL: null,
+      localLoading: false,
+      user: this.getUser(),
     }
+  },
+  methods: {
+    ...mapGetters('authentication', ['getUser']),
+    getURL(file) {
+      if (isNil(file) || typeof file !== 'object') return null
+      return URL.createObjectURL(file)
+    },
+  },
+  asyncComputed: {
+    photoFile() {
+      const storageRef = storage().ref()
+      console.log('storageRef', storageRef)
+    },
   },
 }
