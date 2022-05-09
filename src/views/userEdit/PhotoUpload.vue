d25de7e18b9a3d420ee1a20b485109943376f0ce
MarcoSantana <marco.santana@gmail.com>
2022-05-09 13:29:29 -0500

    Added image upload/preview stump

---
+++ src/views/userEdit/PhotoUpload.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col sm="12" md="6">
+        <v-file-input v-model="imgValue" accept="image/*" label="Archivo (formato PNG ó JPEG)" dense show-size />
+      </v-col>
+      <v-col sm="12" md="6">
+        <v-img max-height="300" max-width="200" :src="url" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+import { isNil } from 'lodash'
+
+export default {
+  data() {
+    return {
+      imgValue: null,
+    }
+  },
+  computed: {
+    url() {
+      return isNil(this.imgValue) ? null : URL.createObjectURL(this.imgValue)
+    },
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
---
+++ src/views/userEdit/profilePhotoPlan.md
@@ -0,0 +1,9 @@
+# Proflie photo plan
+
+## TODO's
+
+- [ ] Connect w/ firebase storage
+- [ ] Create a folder for the user profile photo (maybe user non relevant crap)
+- [ ] Add a v-dialog to offer upload a file or add a URL
+- [ ] Add a image cropper/editor
+- [ ] Webcam capabilities
