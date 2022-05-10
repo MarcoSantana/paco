fee7a507d25cc5702f021ca9f861b6ccecaa7bcc
MarcoSantana <marco.santana@gmail.com>
2022-05-10 11:19:11 -0500

    Working file upload with preview

--- src/views/userEdit/PhotoUpload.vue
+++ src/views/userEdit/PhotoUpload.vue
@@ -2,9 +2,33 @@
   <v-container>
     <v-row>
       <v-col sm="12" md="6">
-        <v-file-input v-model="imgValue" accept="image/*" label="Archivo (formato PNG ó JPEG)" dense show-size />
+        <v-file-input
+          v-model="imgValue"
+          :disabled="uploadLoading"
+          :loading="uploadLoading"
+          accept="image/*"
+          label="Archivo de imagen"
+          dense
+          show-size
+        ></v-file-input>
+        <v-btn
+          v-if="imgValue"
+          :disabled="uploadLoading"
+          :loading="uploadLoading"
+          block
+          outlined
+          color="success"
+          small
+          @click="uploadPhoto(imgValue)"
+        >
+          <v-icon>mdi-cloud-upload</v-icon>
+
+          <template v-slot:loader>
+            <span>Cargando archivo</span>
+          </template>
+        </v-btn>
       </v-col>
-      <v-col sm="12" md="6">
+      <v-col v-if="imgValue" sm="12" md="6">
         <v-img max-height="300" max-width="200" :src="url" />
       </v-col>
     </v-row>
@@ -12,19 +36,62 @@
 </template>
 <script>
 import { isNil } from 'lodash'
+import { auth, storage, firestore } from 'firebase'
+import { mapState } from 'vuex'
 
 export default {
+  name: 'PhotoUpload',
   data() {
     return {
       imgValue: null,
+      uploadLoading: false,
+      loadingPercentage: null,
+      uploadComplete: true,
     }
   },
   computed: {
+    ...mapState('authentication', ['user']),
+    ...mapState('app', ['apiURL']),
     url() {
       return isNil(this.imgValue) ? null : URL.createObjectURL(this.imgValue)
     },
   },
   mounted() {},
-  methods: {},
+  methods: {
+    uploadPhoto(file) {
+      this.uploadLoading = true
+      const fileExtension = file.type.split('/').pop()
+      const storageRef = storage().ref(`users/${this.user.id}/profilePhoto.${fileExtension}`)
+      this.uploadLoading = true
+      this.uploadComplete = false
+      storageRef
+        .put(file)
+        .then(snapshot => {
+          this.loadingPercentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          return snapshot.ref.getDownloadURL()
+        })
+        .then(downloadURL => {
+          this.uploadComplete = true
+          console.log('File available at ', downloadURL)
+        })
+        .then(this.updateProfilePhoto)
+        .catch(err => {
+          console.error('Error al cargar el archivo', err)
+        })
+        .finally(() => (this.uploadLoading = false))
+      // TODO update user state to reflect changes
+      // TODO emit message for toast
+    },
+    async updateProfilePhoto() {
+      console.log('update profile photo')
+      await firestore()
+        .collection('users')
+        .doc(this.user.id)
+        .update({ photoURL: `https://us-central1-paco-1a08b.cloudfunctions.net/profile/${this.user.license}/photo` })
+      await auth().currentUser.updateProfile({
+        photoURL: `https://us-central1-paco-1a08b.cloudfunctions.net/profile${this.user.license}/photo`,
+      })
+    },
+  },
 }
 </script>
