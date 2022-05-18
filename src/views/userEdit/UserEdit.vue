cf646d6954629c5d1233841f41ea3f7c06927ed1
MarcoSantana <marco.santana@gmail.com>
2022-05-18 10:31:33 -0500

    Lint

--- src/views/userEdit/UserEdit.vue
+++ src/views/userEdit/UserEdit.vue
@@ -10,20 +10,57 @@
         <v-card class="px-5" width="100%">
           <validation-observer ref="form" v-slot="{ invalid, changed }">
             <form @submit.prevent="submit">
-              <user-field :model="localUser" disabled name="license" rules="numeric|length:7,10|required" type="text" />
-              <user-field :model="localUser" disabled name="email" rules="required|email" type="email" />
+              <user-field
+                :model="localUser"
+                disabled
+                name="license"
+                rules="numeric|length:7,10|required"
+                type="text"
+              />
+              <user-field
+                :model="localUser"
+                disabled
+                name="email"
+                rules="required|email"
+                type="email"
+              />
               <user-field :model="localUser" name="name" rules="required|length:2,30" type="text" />
-              <user-field :model="localUser" name="lastname1" rules="required|length:2,30" type="text" />
-              <user-field :model="localUser" name="lastname2" rules="required|length:2,30" type="text" />
-              <user-field :model="localUser" name="phoneNumber" rules="required|cellphone" type="tel" />
+              <user-field
+                :model="localUser"
+                name="lastname1"
+                rules="required|length:2,30"
+                type="text"
+              />
+              <user-field
+                :model="localUser"
+                name="lastname2"
+                rules="required|length:2,30"
+                type="text"
+              />
+              <user-field
+                :model="localUser"
+                name="phoneNumber"
+                rules="required|cellphone"
+                type="tel"
+              />
               <v-card-actions class="d-flex justify-center">
                 <v-sheet>
-                  <v-btn :disabled="invalid || !changed" class="mx-3" color="success" outlined tile @click="save">
-                    Guardar cambios
-                  </v-btn>
-                  <v-btn :disabled="!changed" class="mx-3" color="warning" outlined tile @click="resetForm">
-                    Reiniciar formulario
-                  </v-btn>
+                  <v-btn
+                    :disabled="invalid || !changed"
+                    class="mx-3"
+                    color="success"
+                    outlined
+                    tile
+                    @click="save"
+                  >Guardar cambios</v-btn>
+                  <v-btn
+                    :disabled="!changed"
+                    class="mx-3"
+                    color="warning"
+                    outlined
+                    tile
+                    @click="resetForm"
+                  >Reiniciar formulario</v-btn>
                 </v-sheet>
               </v-card-actions>
             </form>
@@ -31,10 +68,22 @@
         </v-card>
       </v-sheet>
     </v-row>
-    <v-snackbar v-model="snackbar.show" absolute centered rounded="pill" color="primary" :timeout="2000">
+    <v-snackbar
+      v-model="snackbar.show"
+      absolute
+      centered
+      rounded="pill"
+      color="primary"
+      :timeout="2000"
+    >
       {{ snackbar.message }}
       <template v-slot:action="{ attrs }">
-        <v-btn color="blue" text v-bind="attrs" @click="snackbar.show = false">{{ $t('actions.close') }}</v-btn>
+        <v-btn
+          color="blue"
+          text
+          v-bind="attrs"
+          @click="snackbar.show = false"
+        >{{ $t('actions.close') }}</v-btn>
       </template>
     </v-snackbar>
   </v-container>
