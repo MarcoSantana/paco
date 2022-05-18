48da5276c9403b3c77d7101aca674c5cb7253d0f
MarcoSantana <marco.santana@gmail.com>
2022-05-18 10:27:23 -0500

    Added snackbar for save and form reset

--- src/views/userEdit/UserEdit.vue
+++ src/views/userEdit/UserEdit.vue
@@ -10,57 +10,20 @@
         <v-card class="px-5" width="100%">
           <validation-observer ref="form" v-slot="{ invalid, changed }">
             <form @submit.prevent="submit">
-              <user-field
-                :model="localUser"
-                disabled
-                name="license"
-                rules="numeric|length:7,10|required"
-                type="text"
-              />
-              <user-field
-                :model="localUser"
-                disabled
-                name="email"
-                rules="required|email"
-                type="email"
-              />
+              <user-field :model="localUser" disabled name="license" rules="numeric|length:7,10|required" type="text" />
+              <user-field :model="localUser" disabled name="email" rules="required|email" type="email" />
               <user-field :model="localUser" name="name" rules="required|length:2,30" type="text" />
-              <user-field
-                :model="localUser"
-                name="lastname1"
-                rules="required|length:2,30"
-                type="text"
-              />
-              <user-field
-                :model="localUser"
-                name="lastname2"
-                rules="required|length:2,30"
-                type="text"
-              />
-              <user-field
-                :model="localUser"
-                name="phoneNumber"
-                rules="required|cellphone"
-                type="tel"
-              />
+              <user-field :model="localUser" name="lastname1" rules="required|length:2,30" type="text" />
+              <user-field :model="localUser" name="lastname2" rules="required|length:2,30" type="text" />
+              <user-field :model="localUser" name="phoneNumber" rules="required|cellphone" type="tel" />
               <v-card-actions class="d-flex justify-center">
                 <v-sheet>
-                  <v-btn
-                    :disabled="invalid || !changed"
-                    @click="save"
-                    class="mx-3"
-                    color="success"
-                    outlined
-                    tile
-                  >Guardar cambios</v-btn>
-                  <v-btn
-                    :disabled="!changed"
-                    @click="resetForm"
-                    class="mx-3"
-                    color="warning"
-                    outlined
-                    tile
-                  >Reiniciar formulario</v-btn>
+                  <v-btn :disabled="invalid || !changed" class="mx-3" color="success" outlined tile @click="save">
+                    Guardar cambios
+                  </v-btn>
+                  <v-btn :disabled="!changed" class="mx-3" color="warning" outlined tile @click="resetForm">
+                    Reiniciar formulario
+                  </v-btn>
                 </v-sheet>
               </v-card-actions>
             </form>
@@ -68,6 +31,12 @@
         </v-card>
       </v-sheet>
     </v-row>
+    <v-snackbar v-model="snackbar.show" absolute centered rounded="pill" color="primary" :timeout="2000">
+      {{ snackbar.message }}
+      <template v-slot:action="{ attrs }">
+        <v-btn color="blue" text v-bind="attrs" @click="snackbar.show = false">{{ $t('actions.close') }}</v-btn>
+      </template>
+    </v-snackbar>
   </v-container>
 </template>
 <script>
@@ -85,18 +54,17 @@ export default {
       errors: null,
       infoDialog: false,
       localSaving: false,
+      snackbar: { show: false, message: '', timeout: 2000 },
       valid: true,
     }
   },
   computed: {
     ...mapState('authentication', ['user']),
     localUser: {
-      // eslint-disable-next-line object-shorthand
-      get: function () {
+      get: function get() {
         return this.getUser()
       },
-      // eslint-disable-next-line object-shorthand
-      set: function (newValue) {
+      set: function set(newValue) {
         return this.setUser(newValue)
       },
     },
@@ -110,9 +78,11 @@ export default {
       this.updateUser(this.localUser)
       this.setUser(this.localUser)
       this.resetForm()
+      this.snackbar = { ...this.snackbar, show: true, message: 'Cambios almacenados' }
     },
     resetForm() {
       this.$refs.form.reset()
+      this.snackbar = { ...this.snackbar, show: true, message: 'Formulario reiniciado' }
       this.localUser = { ...this.user }
     },
   },
