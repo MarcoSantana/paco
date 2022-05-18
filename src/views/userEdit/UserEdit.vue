3e7ec762079edff856f7cbf522fe29f466c1e47e
MarcoSantana <marco.santana@gmail.com>
2022-05-17 23:04:59 -0500

    Form saves as expected and resets from bd

--- src/views/userEdit/UserEdit.vue
+++ src/views/userEdit/UserEdit.vue
@@ -6,25 +6,61 @@
       <v-sheet class="col-sm-12 col-md-4">
         <profile-photo :model="localUser" />
       </v-sheet>
-
       <v-sheet class="col-sm-12 col-md-8">
         <v-card class="px-5" width="100%">
-          <validation-observer v-slot="{ invalid, changed }">
-            {{ (valid = !invalid) }}
-            {{ (localChanged = changed) }}
+          <validation-observer ref="form" v-slot="{ invalid, changed }">
             <form @submit.prevent="submit">
-              <user-field :model="localUser" disabled name="license" rules="numeric|length:7,10|required" type="text" />
-              <user-field :model="localUser" disabled name="email" rules="required|email" type="email" />
+              <user-field
+                :model="localUser"
+                disabled
+                name="license"
+                rules="numeric|length:7,10|required"
+                type="text"
+              />
+              <user-field
+                :model="localUser"
+                disabled
+                name="email"
+                rules="required|email"
+                type="email"
+              />
               <user-field :model="localUser" name="name" rules="required|length:2,30" type="text" />
-              <user-field :model="localUser" name="lastname1" rules="required|length:2,30" type="text" />
-              <user-field :model="localUser" name="lastname2" rules="required|length:2,30" type="text" />
-              <user-field :model="localUser" name="phoneNumber" rules="required|cellphone" type="tel" />
+              <user-field
+                :model="localUser"
+                name="lastname1"
+                rules="required|length:2,30"
+                type="text"
+              />
+              <user-field
+                :model="localUser"
+                name="lastname2"
+                rules="required|length:2,30"
+                type="text"
+              />
+              <user-field
+                :model="localUser"
+                name="phoneNumber"
+                rules="required|cellphone"
+                type="tel"
+              />
               <v-card-actions class="d-flex justify-center">
                 <v-sheet>
-                  <v-btn class="mx-3" tile color="success" outlined :disabled="invalid || !localChanged" @click="save">
-                    Guardar cambios
-                  </v-btn>
-                  <v-btn class="mx-3" tile color="warning" outlined :disabled="!localChanged">Borrar formulario</v-btn>
+                  <v-btn
+                    :disabled="invalid || !changed"
+                    @click="save"
+                    class="mx-3"
+                    color="success"
+                    outlined
+                    tile
+                  >Guardar cambios</v-btn>
+                  <v-btn
+                    :disabled="!changed"
+                    @click="resetForm"
+                    class="mx-3"
+                    color="warning"
+                    outlined
+                    tile
+                  >Reiniciar formulario</v-btn>
                 </v-sheet>
               </v-card-actions>
             </form>
@@ -48,7 +84,6 @@ export default {
     return {
       errors: null,
       infoDialog: false,
-      localChanged: false,
       localSaving: false,
       valid: true,
     }
@@ -57,23 +92,16 @@ export default {
     ...mapState('authentication', ['user']),
     localUser: {
       // eslint-disable-next-line object-shorthand
-      get: function() {
+      get: function () {
         return this.getUser()
       },
       // eslint-disable-next-line object-shorthand
-      set: function(newValue) {
-        return this.setUser(...newValue)
+      set: function (newValue) {
+        return this.setUser(newValue)
       },
     },
   },
-
-  mounted() {
-    // console.log('refs.phoneNumber', this.$refs.phoneNumber.disabled)
-    // this.localUser = this.getUser()
-    //  console.log('localUser', this.localUser)
-    //  this.localUser = Object.assign(this.localUser, this.getUser())
-    //  console.log('localUser', this.localUser)
-  },
+  mounted() {},
   methods: {
     ...mapMutations('authentication', ['setUser']),
     ...mapGetters('authentication', ['getUser']),
@@ -81,7 +109,11 @@ export default {
     save() {
       this.updateUser(this.localUser)
       this.setUser(this.localUser)
-      this.localChanged = false
+      this.resetForm()
+    },
+    resetForm() {
+      this.$refs.form.reset()
+      this.localUser = { ...this.user }
     },
   },
 }
