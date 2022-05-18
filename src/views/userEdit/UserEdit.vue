e03d18c7b508bebc7dc6ade891884a9971c913c0
MarcoSantana <marco.santana@gmail.com>
2022-05-17 21:47:09 -0500

    localUser updates when save

--- src/views/userEdit/UserEdit.vue
+++ src/views/userEdit/UserEdit.vue
@@ -4,63 +4,27 @@
     <info-dialog />
     <v-row>
       <v-sheet class="col-sm-12 col-md-4">
-        <profile-photo :model="user" />
+        <profile-photo :model="localUser" />
       </v-sheet>
 
       <v-sheet class="col-sm-12 col-md-8">
         <v-card class="px-5" width="100%">
           <validation-observer v-slot="{ invalid, changed }">
+            {{ (valid = !invalid) }}
+            {{ (localChanged = changed) }}
             <form @submit.prevent="submit">
-              <user-field
-                :model="localUser"
-                disabled
-                name="license"
-                rules="numeric|length:7,10|required"
-                type="text"
-              />
-              <user-field
-                :model="localUser"
-                disabled
-                name="email"
-                rules="required|email"
-                type="email"
-              />
+              <user-field :model="localUser" disabled name="license" rules="numeric|length:7,10|required" type="text" />
+              <user-field :model="localUser" disabled name="email" rules="required|email" type="email" />
               <user-field :model="localUser" name="name" rules="required|length:2,30" type="text" />
-              <user-field
-                :model="localUser"
-                name="lastname1"
-                rules="required|length:2,30"
-                type="text"
-              />
-              <user-field
-                :model="localUser"
-                name="lastname2"
-                rules="required|length:2,30"
-                type="text"
-              />
-              <user-field
-                :model="localUser"
-                name="phoneNumber"
-                rules="required|cellphone"
-                type="tel"
-              />
+              <user-field :model="localUser" name="lastname1" rules="required|length:2,30" type="text" />
+              <user-field :model="localUser" name="lastname2" rules="required|length:2,30" type="text" />
+              <user-field :model="localUser" name="phoneNumber" rules="required|cellphone" type="tel" />
               <v-card-actions class="d-flex justify-center">
                 <v-sheet>
-                  <v-btn
-                    class="mx-3"
-                    tile
-                    color="success"
-                    outlined
-                    :disabled="invalid || !changed"
-                    @click="save"
-                  >Guardar cambios</v-btn>
-                  <v-btn
-                    class="mx-3"
-                    tile
-                    color="warning"
-                    outlined
-                    :disabled="!changed"
-                  >Borrar formulario</v-btn>
+                  <v-btn class="mx-3" tile color="success" outlined :disabled="invalid || !localChanged" @click="save">
+                    Guardar cambios
+                  </v-btn>
+                  <v-btn class="mx-3" tile color="warning" outlined :disabled="!localChanged">Borrar formulario</v-btn>
                 </v-sheet>
               </v-card-actions>
             </form>
@@ -84,13 +48,23 @@ export default {
     return {
       errors: null,
       infoDialog: false,
+      localChanged: false,
       localSaving: false,
-      localUser: this.getUser(),
       valid: true,
     }
   },
   computed: {
     ...mapState('authentication', ['user']),
+    localUser: {
+      // eslint-disable-next-line object-shorthand
+      get: function() {
+        return this.getUser()
+      },
+      // eslint-disable-next-line object-shorthand
+      set: function(newValue) {
+        return this.setUser(...newValue)
+      },
+    },
   },
 
   mounted() {
@@ -105,9 +79,9 @@ export default {
     ...mapGetters('authentication', ['getUser']),
     ...mapActions('user', ['updateUser']),
     save() {
-      this.localSaving = true
       this.updateUser(this.localUser)
-      this.localSaving = false
+      this.setUser(this.localUser)
+      this.localChanged = false
     },
   },
 }
