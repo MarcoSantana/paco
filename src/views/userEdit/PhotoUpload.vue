a81aa7168ffe1409a6df8795c79b8fd57bfbc57a
MarcoSantana <marco.santana@gmail.com>
2022-05-18 22:07:04 -0500

    Improved preview and upload button

--- src/views/userEdit/PhotoUpload.vue
+++ src/views/userEdit/PhotoUpload.vue
@@ -1,7 +1,20 @@
 <template>
   <v-container>
     <v-row>
-      <v-col sm="12" md="6">
+      <v-col sm="12" md="12">
+        <v-btn
+          v-if="imgValue"
+          :disabled="uploadLoading"
+          :loading="uploadLoading"
+          color="success"
+          large
+          block
+          @click="uploadPhoto(imgValue)"
+        >
+          <v-icon>mdi-cloud-upload</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col sm="12" md="12">
         <v-file-input
           v-model="imgValue"
           :disabled="uploadLoading"
@@ -11,6 +24,8 @@
           dense
           show-size
         ></v-file-input>
+      </v-col>
+      <v-col sm="12" md="6">
         <cropper
           :src="url"
           :stencil-props="{
@@ -18,27 +33,16 @@
           }"
           @change="change"
         ></cropper>
-        <v-btn
-          v-if="imgValue"
-          :disabled="uploadLoading"
-          :loading="uploadLoading"
-          block
-          outlined
-          color="success"
-          small
-          @click="uploadPhoto(imgValue)"
-        >
-          <v-icon>mdi-cloud-upload</v-icon>
-
-          <template v-slot:loader>
-            <span>Cargando archivo</span>
-          </template>
-        </v-btn>
       </v-col>
-      <v-col v-if="imgValue" sm="12" md="6">
+      <v-col class="d-none d-sm-none d-md-flex" sm="12" md="6">
         <v-img ref="photo" max-height="300" max-width="200" :src="result" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 <script src="./photoUpload.comp.js" />
+<style scoped>
+cropper {
+  max-height: 300px;
+}
+</style>
