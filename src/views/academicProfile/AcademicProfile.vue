6cc4700d47782e80f31b38e53201704a2e381360
MarcoSantana <marco.santana@gmail.com>
2022-05-26 17:30:07 -0500

    Format and placeholder text

--- src/views/academicProfile/AcademicProfile.vue
+++ src/views/academicProfile/AcademicProfile.vue
@@ -1,40 +1,42 @@
 <template>
-<v-container>
-  <h3 class="text-h3 pb-3">Perfil académico</h3>
-
-  <v-row>
-    <v-col cols="sm">
-      <v-card>
-        <v-card-title class="text-h4">Licenciatura</v-card-title>
-        <v-list>
-          <v-list-item v-for="(item, key) in academicProfile.license" :key="key">
-            <v-list-item-content class="px-5 text-capitalize">
-              <v-list-item-title class="text-h5">{{ item | genderize | missingData }}</v-list-item-title>
-              {{ $t(`academicProfile.license.${key}`, {}) }}
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-card>
-    </v-col>
-    <v-col cols="sm">
-      <v-card>
-        <v-card-title>Especialidad</v-card-title>
-        <v-list v-if="academicProfile.posgraduateLicense">
-          <v-list-item v-for="(item, key) in academicProfile.posgraduateLicense" :key="key">
-            <v-list-item-content class="px-5 text-capitalize">
-              <v-list-item-title class="text-h5">{{ item | genderize | missingData }}</v-list-item-title>
-              {{ $t(`academicProfile.${key}`, {}) }}
-            </v-list-item-content>
-          </v-list-item>
-          <v-divider inset></v-divider>
-        </v-list>
-        <v-card>
-            Agregar datos de cédula profesional de especialista
+  <v-container>
+    <h3 class="text-h3 pb-3">Perfil académico</h3>
+    <v-row>
+      <v-col cols="sm">
+        <v-card class="pa-3">
+          <v-card-title class="text-h4">Licenciatura</v-card-title>
+          <v-list>
+            <v-list-item v-for="(item, key) in academicProfile.license" :key="key">
+              <v-list-item-content class="px-5 text-capitalize">
+                <v-list-item-title class="text-h5">{{ item | genderize | missingData }}</v-list-item-title>
+                {{ $t(`academicProfile.license.${key}`, {}) }}
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
         </v-card>
-      </v-card>
-    </v-col>
-  </v-row>
-</v-container>
+      </v-col>
+      <v-col cols="sm">
+        <v-card class="pa-3">
+          <v-card-title>Especialidad</v-card-title>
+          <v-list v-if="academicProfile.posgraduateLicense">
+            <v-list-item v-for="(item, key) in academicProfile.posgraduateLicense" :key="key">
+              <v-list-item-content class="px-5 text-capitalize">
+                <v-list-item-title class="text-h5">{{ item | genderize | missingData }}</v-list-item-title>
+                {{ $t(`academicProfile.${key}`, {}) }}
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider inset></v-divider>
+          </v-list>
+          <v-card v-else>
+            <v-card-text>
+              Agregar datos de cédula profesional de especialista
+              TODO Agregar un componente para hacer esto más fácil
+            </v-card-text>
+          </v-card>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 import { mapState } from 'vuex'
@@ -44,7 +46,7 @@ export default {
   name: 'AcademicProfile',
 
   filters: {
-    genderize: val => {
+    genderize: (val) => {
       if (isNil(val) || val.isNaN) return val
       switch (val) {
         case '1':
@@ -55,7 +57,7 @@ export default {
           return val
       }
     },
-    missingData: val => {
+    missingData: (val) => {
       if (isNil(val)) return 'Datos faltantes'
       return val
     },
