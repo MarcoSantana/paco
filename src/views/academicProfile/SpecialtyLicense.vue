e0d470f7b5b9771dd934e72ab3047b6c594fbc47
MarcoSantana <marco.santana@gmail.com>
2022-05-31 17:15:31 -0500

    Fixed watch debounced event

--- src/views/academicProfile/SpecialtyLicense.vue
+++ src/views/academicProfile/SpecialtyLicense.vue
@@ -3,9 +3,8 @@
     <ul>
       <li><h3>TODO</h3></li>
       <li>Add update button</li>
-      <li>Add "Why I am seeing this?"</li>
-      <li>Add a tooltip to explain the license search</li>
-      <li>Hide the search button or better if there is a cancelled version of the icon for the invalid state</li>
+      <li>Add "Why I am seeing this?" (put this in parent component)</li>
+      <li>Add a tooltip to explain the license search (put this in parent component)</li>
       <li>Validate field</li>
       <li>Refactor name and reuse it in signup</li>
       <li>As part of the refactor move it to components directory</li>
@@ -18,12 +17,16 @@
         </v-card-text>
       </v-card>
     </v-dialog>
-    <v-text-field
-      v-model="licenseNumber"
-      clearable
-      :append-icon="`mdi-search-web`"
-      @click:append="debouncedLicenseCheck"
-    />
+    <v-text-field v-model="licenseNumber" clearable>
+      <template v-slot:append @click="debouncedLicenseCheck">
+        <v-fade-transition leave-absolute>
+          <v-progress-circular v-if="loading" size="24" color="info" indeterminate></v-progress-circular>
+          <v-icon v-else left>
+            mdi-search-web
+          </v-icon>
+        </v-fade-transition>
+      </template>
+    </v-text-field>
   </v-container>
 </template>
 <script>
@@ -39,6 +42,15 @@ export default {
   }),
   computed: { ...mapState('academicProfile', ['academicProfile']) },
   watch: {
+    licenseNumber: function licenseNumber() {
+      if (isNil(this.licenseNumber)) {
+        this.licenseData = null
+        return null
+      }
+      if (Number.isNaN(this.licenseNumber)) return null
+      this.debouncedLicenseCheck(this)
+      return true
+    },
     licenseData: function licenseData(val) {
       return this.$emit('license', val)
     },
@@ -49,8 +61,8 @@ export default {
       this.licenseCheck(this)
     }, 1000),
     licenseCheck: async that => {
-      console.log('licenseCheck')
-      if (isNil(that.licenseNumber) && Number.isNaN(that.licenseNumber)) return null
+      if (isNil(that.licenseNumber)) return null
+      if (Number.isNaN(that.licenseNumber)) return null
       if (that.licenseNumber.length < 7 || that.licenseNumber.length > 10) return null
       that.loading = true
       const myResponse = await fetch(
