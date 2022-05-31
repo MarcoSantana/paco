afd452e3fd1793fd4ec4036a5b0418b48cac51c3
MarcoSantana <marco.santana@gmail.com>
2022-05-31 10:45:56 -0500

    Abandoned automatic search in favor of button

--- src/views/academicProfile/SpecialtyLicense.vue
+++ src/views/academicProfile/SpecialtyLicense.vue
@@ -1,14 +1,16 @@
 <template>
   <v-container>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure hic voluptates unde ab, cupiditate labore non esse.
-    Illo quae asperiores unde, aspernatur quaerat consequatur aperiam distinctio blanditiis ut, numquam eveniet? Lorem
-    ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta fuga impedit iste cumque animi ipsum quia, ex
-    blanditiis rem nulla unde odit. Ipsa nostrum assumenda ex quos, nihil nobis?
     <ul>
+      <li><h3>TODO</h3></li>
       <li>Add update button</li>
       <li>Add "Why I am seeing this?"</li>
+      <li>Add a tooltip to explain the license search</li>
+      <li>Hide the search button or better if there is a cancelled version of the icon for the invalid state</li>
+      <li>Validate field</li>
+      <li>Refactor name and reuse it in signup</li>
+      <li>As part of the refactor move it to components directory</li>
     </ul>
-    <v-dialog v-model="$asyncComputed.licenseData.updating" max-width="290">
+    <v-dialog v-model="loading" max-width="290">
       <v-card color="primary" dark>
         <v-card-text>
           Cargando datos
@@ -16,17 +18,24 @@
         </v-card-text>
       </v-card>
     </v-dialog>
-    <v-text-field v-model="licenseNumber" clearable />
+    <v-text-field
+      v-model="licenseNumber"
+      clearable
+      :append-icon="`mdi-search-web`"
+      @click:append="debouncedLicenseCheck"
+    />
   </v-container>
 </template>
 <script>
 import { mapState, mapMutations } from 'vuex'
-import { isNil } from 'lodash'
+import { isNil, debounce } from 'lodash'
 
 export default {
   name: 'SpecialtyLicense',
   data: () => ({
     licenseNumber: '',
+    loading: false,
+    licenseData: null,
   }),
   computed: { ...mapState('academicProfile', ['academicProfile']) },
   watch: {
@@ -34,21 +43,23 @@ export default {
       return this.$emit('license', val)
     },
   },
-  methods: { ...mapMutations('academicProfile', ['updateAcademicProfile']) },
-  asyncComputed: {
-    licenseData: {
-      async get() {
-        if (isNil(this.licenseNumber) || Number.isNaN(this.licenseNumber)) return null
-        if (this.licenseNumber.length < 7 || this.licenseNumber.length > 10) return null
-        // TODO Debouncer me
-        console.log('this.licenseNumber', this.licenseNumber)
-        const val = this.licenseNumber
-        const response = await fetch(
-          `https://us-central1-paco-1a08b.cloudfunctions.net/licenseAPI-licenseAPI/${val}`,
-          {}
-        )
-        return response.json()
-      },
+  methods: {
+    ...mapMutations('academicProfile', ['updateAcademicProfile']),
+    debouncedLicenseCheck: debounce(function bar() {
+      this.licenseCheck(this)
+    }, 1000),
+    licenseCheck: async that => {
+      console.log('licenseCheck')
+      if (isNil(that.licenseNumber) && Number.isNaN(that.licenseNumber)) return null
+      if (that.licenseNumber.length < 7 || that.licenseNumber.length > 10) return null
+      that.loading = true
+      const myResponse = await fetch(
+        `https://us-central1-paco-1a08b.cloudfunctions.net/licenseAPI-licenseAPI/${that.licenseNumber}`,
+        {}
+      )
+      that.loading = false
+      that.licenseData = await myResponse.json()
+      return myResponse
     },
   },
 }
