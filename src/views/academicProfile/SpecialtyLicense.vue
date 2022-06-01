68191b2bd883cdbdf8f61bce399c05c197538040
MarcoSantana <marco.santana@gmail.com>
2022-06-01 16:58:23 -0500

    Added help button for field

--- src/views/academicProfile/SpecialtyLicense.vue
+++ src/views/academicProfile/SpecialtyLicense.vue
@@ -8,24 +8,32 @@
         </v-card-text>
       </v-card>
     </v-dialog>
-
+    <v-dialog v-model="help" max-width="290">
+      <v-card>
+        <v-card-title class="text-h5 primary lighten-2 white--text">¿Qué es este campo?</v-card-title>
+        <v-card-text class="pt-2">
+          <p>Aquí debe de capturar su cédula profesional</p>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="help = false">Entiendo</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <validation-observer>
       <validation-provider v-slot="{ errors }" rules="numeric|length:7,10">
         <v-text-field v-model="licenseNumber" clearable>
           <template v-slot:append @click="debouncedLicenseCheck">
             <v-fade-transition leave-absolute>
               <v-progress-circular v-if="loading" size="24" color="info" indeterminate></v-progress-circular>
-              <v-icon v-else left>
-                mdi-search-web
-              </v-icon>
+              <v-icon v-else left>mdi-search-web</v-icon>
             </v-fade-transition>
           </template>
           <template v-slot:prepend>
             <v-tooltip bottom>
               <template v-slot:activator="{ on, attrs }">
-                <v-icon color="info" v-bind="attrs" v-on="on">
-                  mdi-information
-                </v-icon>
+                <v-icon color="info" v-bind="attrs" v-on="on" @click="help = true">mdi-information</v-icon>
               </template>
               <span>Búsqueda de cédula profesional</span>
             </v-tooltip>
@@ -34,14 +42,6 @@
         <span class="error--text error lighten-4">{{ errors[0] }}</span>
       </validation-provider>
     </validation-observer>
-    <ul>
-      <h3>TODO</h3>
-      <li>Add update button (put this in parent component)</li>
-      <li>Add "Why I am seeing this?" (put this in parent component)</li>
-      <li>Add a tooltip to explain the license search (put this in parent component)</li>
-      <li>Refactor name and reuse it in signup</li>
-      <li>As part of the refactor move it to components directory</li>
-    </ul>
   </v-container>
 </template>
 <script>
@@ -51,6 +51,7 @@ import { isNil, debounce } from 'lodash'
 export default {
   name: 'SpecialtyLicense',
   data: () => ({
+    help: false,
     licenseNumber: '',
     loading: false,
     licenseData: null,
@@ -79,7 +80,7 @@ export default {
       if (this.licenseNumber.length < 7 || this.licenseNumber.length > 10) return null
       return this.licenseCheck(this)
     }, 250),
-    licenseCheck: async that => {
+    licenseCheck: async (that) => {
       that.loading = true
       const myResponse = await fetch(
         `https://us-central1-paco-1a08b.cloudfunctions.net/licenseAPI-licenseAPI/${that.licenseNumber}`,
