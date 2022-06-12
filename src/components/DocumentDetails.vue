50f126558ce19a70c307f8d27336ec7cb742a410
MarcoSantana <marco.santana@gmail.com>
2022-06-12 10:53:23 -0500

    Created new component working (stub)

--- src/components/DocumentDetails.vue
+++ src/components/DocumentDetails.vue
@@ -1,17 +1,54 @@
 <template>
-  <div class="document-detail">
-    <h1>{{ document.name }}</h1>
-    <div>{{ document.data }}</div>
-    <div>
-      <!-- <img width="500px" :src="document.data.avatar" alt="" /> -->
-    </div>
-  </div>
+  <v-container>
+    <v-card class="pa-3">
+      <v-card-title>
+        <h1 class="text-capitalize text-h4">{{$t(`document.types.${document.name}`)}}</h1>
+      </v-card-title>
+      <show-file v-for="file in docURLs" :key="file" :url="file" @removeFile="file = null" />
+    </v-card>
+  </v-container>
 </template>
 
 <script>
+import { isNil } from 'lodash'
+import { storage } from 'firebase'
+import ShowFile from '@/components/ShowFile'
+
 export default {
+  name: 'DocumentDetail',
+  components: { ShowFile },
   props: {
     document: Object,
+  },
+  data() {
+    return {
+      docURLs: [],
+    }
+  },
+  mounted() {
+    console.log('Document detail', this.document)
+    this.$nextTick(() => {
+      this.populateRemoteFiles(this.documentFiles)
+    })
+  },
+  methods: {
+    populateRemoteFiles(files) {
+      if (isNil(files)) return null
+      return files.map(async (file) => {
+        this.docURLs.push(await this.getDownloadURL(file))
+      })
+    },
+
+    async getDownloadURL(docRef) {
+      const storageRef = storage().ref(docRef)
+      const url = await storageRef.getDownloadURL()
+      return url
+    },
+  },
+  computed: {
+    documentFiles() {
+      return Object.values(this.document.files)
+    },
   },
 }
 </script>
