da7aaac23cdf87abeec704c7488e9b2e4a57073c
MarcoSantana <marco.santana@gmail.com>
2022-06-13 13:48:28 -0500

    Fixed prop typo

---
+++ src/components/forms/CommentField.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-sheet>
+    <v-text-field
+      label="Comentario(opcional)"
+      :placeholder="placeholder"
+      outlined
+      @input="input"
+    ></v-text-field>
+  </v-sheet>
+</template>
+
+<script>
+// import { VTextField } from 'vuetify/lib'
+
+export default {
+  name: 'CommentField',
+  // components: { VTextField },
+  props: {
+    placeholder: {
+      type: String,
+      default: 'Comentario',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    input(e) {
+      this.$emit('input', e)
+    },
+  },
+}
+</script>
+
+<style></style>
