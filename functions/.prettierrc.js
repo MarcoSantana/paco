5d2b716d95d3c06d2538fc7aeb43c3aae240cdd4
MarcoSantana <marco.santana@gmail.com>
2022-06-24 11:23:32 -0500

    Updated configs

---
+++ functions/.prettierrc.js
@@ -0,0 +1,11 @@
+// .prettierrc.js
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    trailingComma: "all",
+    printWidth: 80,
+    tabWidth: 2,
+    useTabs: false,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+}
