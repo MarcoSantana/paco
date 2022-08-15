6de9a34d8a9d7694b0cf2d26c1d53d2b0ae9657a
MarcoSantana <marco.santana@gmail.com>
2022-08-15 09:16:05 -0500

    Refacotored to extract field to own component

---
+++ src/components/admin/dialogs/DobField.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-sheet>
+    <v-date-picker
+      v-if="show"
+      v-model="localDOB"
+      :rules="rules"
+      :label="$t('userData.personalProfile.dob')"
+      required
+      @change="$emit('setDOB', localDOB)"
+    ></v-date-picker>
+  </v-sheet>
+</template>
+<script>
+//@ts-check
+/**
+ *@vue-prop {String} dob - The dob to start with
+ *@vue-prop {Boolean} show - Whether to show the field
+ *@vue-prop {Array} rules - Validation rules
+ *@vue-data {String} dob
+ */
+export default {
+  name: 'DobField',
+  props: {
+    dob: {
+      type: String,
+      required: false,
+    },
+    show: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    rules: {
+      type: Array,
+      required: false,
+    },
+  }, // propsData
+  data: () => ({
+    localDOB: '',
+  }),
+}
+</script>
