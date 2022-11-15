91b4e673967631e7ed6abdde09fbe264983f11d6
MarcoSantana <marco.santana@gmail.com>
2022-11-15 07:54:59 -0600

    Shows a list of the pending docuements

---
+++ src/components/admin/documents/ListUserDocuments.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container>
+    <v-card flat color="transparent" light>
+      <v-card-title primary-title>
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              Documentos pendientes
+              <template v-slot:actions>
+                <v-badge
+                  color="red"
+                  class="mx-3 mt-2"
+                  :content="pendingDocuments.length"
+                ></v-badge>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-card-text>
+                <v-list two-line>
+                  <v-list-item
+                    v-for="document in pendingDocuments"
+                    :key="document.id"
+                  >
+                    <v-list-item-content>
+                      <v-list-item-title>
+                        {{ document.name }}
+                      </v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list>
+              </v-card-text>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-card-title>
+    </v-card>
+  </v-container>
+</template>
+<script lang="js">
+//@ts-check
+
+import { mapActions, mapState, mapGetters, mapMutations} from 'vuex'
+
+export default {
+    name: 'ListUserDocuments', // mounted
+    components: {}, // components
+    props: {
+        user: { type: Object, required: true},
+        // TODO add tag
+    }, // props
+    computed: {
+      ...mapState('documents', ['documents']),
+      ...mapGetters('documents', ['pendingDocuments']),
+    }, //computed
+    mounted: function() {
+      this.resetDocuments
+      this.getUserDocumentsById(this.user.id)
+    }, // mounted
+    methods: {
+        ...mapActions('documents', [
+            'getUserDocumentsById'
+        ]),
+       ...mapMutations('documents', ['resetDocuments'])
+    }, // methods
+
+}
+</script>
